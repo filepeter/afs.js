25488b9f818c87dfa7a82cd9a898f9ca6e507335
Dan Sutherland <djs@djs.org.uk>
2011-06-09 16:11:18 +0100

    Add DataView emulation class for FF, php script to return single sectors

---
+++ getSect.php
@@ -0,0 +1,25 @@
+<?php
+
+define('SECT_SIZE', 512);
+define('MAX_SECT', 1760);
+
+if (! isset($_GET['sect'])) {
+	die();
+}
+
+$sect = (int) $_GET['sect'];
+
+if ($sect < 0 || $sect >= MAX_SECT) {
+	die();
+}
+
+header('Content-type: application/octet-stream');
+
+$fil = fopen('test.adf', 'rb');
+fseek($fil, $sect * SECT_SIZE);
+$buf = fread($fil, SECT_SIZE);
+fclose($fil);
+
+echo $buf;
+
+?>
